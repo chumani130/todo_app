661ce2e0cb26d714947fc9aacb2f92c55107e6f1
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-01-29 13:20:09 +0200

    Update default.component.ts

--- src/app/shared/layouts/default/default.component.ts
+++ src/app/shared/layouts/default/default.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-default',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './default.component.html',
   styleUrl: './default.component.scss'
 })
