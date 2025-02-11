d5392f026645d9e030871ed20060860b717b9721
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-02-11 16:58:15 +0200

    Update todo.component.ts

--- src/app/pages/todo/todo.component.ts
+++ src/app/pages/todo/todo.component.ts
@@ -2,17 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { TodoCardComponent } from '../../shared/components/todo-card/todo-card.component';
 import { TodoService } from '../../core/services/todo.service';
 import { ITodo } from '../../core/models/todo.model';
+import { SlidePanelComponent } from '../../shared/ui/slide-panel/slide-panel.component';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [TodoCardComponent],
+  imports: [TodoCardComponent, SlidePanelComponent],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss'
 })
 export class TodoComponent implements OnInit {
+  todoForm!: FormGroup;
+
   todos: ITodo[] = [];
-  constructor(private todoService: TodoService) {}
+  isSlidePanelOpen = false;
+  constructor(private todoService: TodoService, private fb: FormBuilder) {
+    this.todoForm = this.fb.group({
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      status: new FormControl('OPEN', [Validators.required])
+    })
+  }
 
   ngOnInit() {
     this.getAllTodos();
@@ -20,5 +31,13 @@ export class TodoComponent implements OnInit {
 
   getAllTodos() {
    this.todos =  this.todoService.getAllTodo();
+  }
+
+  onCloseSlidePanel() {
+    this.isSlidePanelOpen = false;
+  }
+
+  openSlidePanel() {
+    this.isSlidePanelOpen = true;
   }
 }
