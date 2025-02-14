15e996076e16a49ea9fd7b637df8580d46b847a4
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-02-14 11:44:51 +0200

    Update login.component.ts

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,7 @@ export class LoginComponent {
 
   loginForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private authService: AuthService) {
     this.loginForm = this.fb.group({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required])
@@ -22,7 +23,11 @@ export class LoginComponent {
   onSubmit() {
     console.log(this.loginForm.value);
     if(this.loginForm.valid) {
-      //call login service
+      this.authService.onLogin(this.loginForm.value).subscribe({
+        next: () => {
+          console.log('Login Success');
+        }
+      });
     } else{
       this.loginForm.markAllAsTouched();
     }
