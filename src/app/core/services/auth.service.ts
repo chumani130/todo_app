06e804f4c36307d407432b17c5e7daf8f1e9171e
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-02-14 11:44:44 +0200

    Update auth.service.ts

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { ILogin, ILoginResponse } from '../models/auth.model';
+import { HttpClient } from '@angular/common/http';
+import { apiEndpoint } from '../constants/constants';
+import { map } from 'rxjs';
+import { TokenService } from './token.service';
+import { response } from 'express';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private tokenService: TokenService) { }
+
+  onLogin(data: ILogin) {
+    return this.http.post<ILoginResponse>(`${apiEndpoint.AuthEndPoint.login}`, data).pipe(
+      map((response) => {
+        if(response) {
+          this.tokenService.setToken(response.token);
+        }
+        return response;
+      })
+    )
+  }
+
+  onLogout() {
+    this.http.post(`${apiEndpoint.AuthEndPoint.logout}`, ''). subscribe({
+      next: (response) => {
+        this.tokenService.removeToken();
+      },
+    })
+  }
 }
